2ac322672e3315d6199dd8020bf16f4e580b7f33
Michael Soh <2767724+sohmc@users.noreply.github.com>
2021-03-06 19:07:26 -0500

    updating binary test.

---
+++ ci/test_binary.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+BIN_NAME=./dist/${BIN_NAME}
+
+echo "Testing ${BIN_NAME}""
+
+set -ev
+
+# echo "Getting binary from S3..."
+# curl -v $S3_URI -o $BIN_NAME
+
+echo "Building template..."
+bash ci/create_template.bash
+
+echo "Running script..."
+${BIN_NAME} -f -c ci/aws_sg_ddns.conf
